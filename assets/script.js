3b753d18603f719394789765eb5cfdbb5bd95cbd
Roro21007 <rharris21007@gmail.com>
2023-10-25 22:39:26 -0500

    initialize and add starting notes

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+// call api to json
+// save to previous cities entered
+// display current city, day, icon, temperature, humidity, and wind speed
+// diplay next 5 dates of current city icon, temerature, humidity, and wind speed
